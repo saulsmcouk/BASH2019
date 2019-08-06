c88060c525ca220e5f2135a1889875a21f28b7c6
Saul Samuels Moselle <SaulSamuelsMoselle@hotmail.com>
2019-08-06 15:43:54 +0100

    Refactored Theo's hard and very much appreciated work. We love you Theo!

--- src/geolocation.js
+++ src/geolocation.js
@@ -4,40 +4,6 @@
 var apikey = '3e343658c7664c0b9b0758ebf7de5472';
 var api_url = 'https://api.opencagedata.com/geocode/v1/json'
 
-function getLatLng(postcode) {
-    var lat = '';
-    var lng = '';
-    var request_url = api_url
-        + '?'
-        + 'key=' + apikey
-        + '&q=' + encodeURIComponent(postcode)
-        + '&pretty=1'
-        + '&no_annotations=1';
-    var request = new XMLHttpRequest();
-    request.open('GET', request_url, true);
-    request.onload = function() {
-        if (request.status == 200){ 
-            var data = JSON.parse(request.responseText);
-            lat = data.results[0].geometry.lat;
-            lng = data.results[0].geometry.lng;
-    
-        } else if (request.status <= 500){                  
-            console.log("unable to geocode! Response code: " + request.status);
-            var data = JSON.parse(request.responseText);
-            console.log(data.status.message);
-        } else {
-            console.log("server error");
-        }
-      };
-    
-    request.onerror = function() {
-        console.log("unable to connect to server");        
-    };
-    
-    request.send();
-    return [lat,lng];
-}
-
 function GenGeoJSON(locs) {
     // Spec: https://tools.ietf.org/html/rfc7946 (see section 1.5 for an example)
     var theData = {
