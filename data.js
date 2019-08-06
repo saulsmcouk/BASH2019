dc2c7d64c8aeae56596d8e7c6c2e57fbb4599aaa
Saul Samuels Moselle <SaulSamuelsMoselle@hotmail.com>
2019-08-06 11:07:13 +0100

    Added a basic function to get postcodes - currently limited because the api only lets you get 50 datapoints at a time and because individuals don't have to list their addresses

--- data.js
+++ data.js
@@ -4,7 +4,7 @@ function LoadECDonationData(callback) {
 	// var theQUery = "currentPage=1&rows=10&sort=AcceptedDate&order=desc&tab=1&open=filter&et=pp&et=ppm&et=tp&et=perpar&et=rd&isIrishSourceYes=true&isIrishSourceNo=true&date=Reported&from=&to=&quarters=${quarters}&prePoll=true&postPoll=true&donorStatus=company&register=none&register=gb&optCols=Register&optCols=CampaigningName&optCols=AccountingUnitsAsCentralParty&optCols=IsSponsorship&optCols=IsIrishSource&optCols=RegulatedDoneeType&optCols=CompanyRegistrationNumber&optCols=Postcode&optCols=NatureOfDonation&optCols=PurposeOfVisit&optCols=DonationAction&optCols=ReportedDate&optCols=IsReportedPrePoll&optCols=ReportingPeriodName&optCols=IsBequest&optCols=IsAggregation";
 	// var theOverall = basicURL + theQUery;
 	// //***TEMP
-	var theOverall = "http://search.electoralcommission.org.uk/api/Search/Donations?currentPage=1&rows=10&sort=AcceptedDate&order=desc&tab=1&et=pp&et=ppm&et=tp&et=perpar&et=rd&isIrishSourceYes=true&isIrishSourceNo=true&prePoll=false&postPoll=true&register=gb&register=ni&register=none&optCols=Register&optCols=CampaigningName&optCols=AccountingUnitsAsCentralParty&optCols=IsSponsorship&optCols=IsIrishSource&optCols=RegulatedDoneeType&optCols=CompanyRegistrationNumber&optCols=Postcode&optCols=NatureOfDonation&optCols=PurposeOfVisit&optCols=DonationAction&optCols=ReportedDate&optCols=IsReportedPrePoll&optCols=ReportingPeriodName&optCols=IsBequest&optCols=IsAggregation";
+	var theOverall = "http://search.electoralcommission.org.uk/api/Search/Donations?currentPage=1&rows=64349&sort=AcceptedDate&order=desc&tab=1&et=pp&et=ppm&et=tp&et=perpar&et=rd&isIrishSourceYes=true&isIrishSourceNo=true&prePoll=false&postPoll=true&register=gb&register=ni&register=none&optCols=Register&optCols=CampaigningName&optCols=AccountingUnitsAsCentralParty&optCols=IsSponsorship&optCols=IsIrishSource&optCols=RegulatedDoneeType&optCols=CompanyRegistrationNumber&optCols=Postcode&optCols=NatureOfDonation&optCols=PurposeOfVisit&optCols=DonationAction&optCols=ReportedDate&optCols=IsReportedPrePoll&optCols=ReportingPeriodName&optCols=IsBequest&optCols=IsAggregation";
 	
 	var response = fetch(theOverall, {
 		"content-type": "application/json"
@@ -27,3 +27,11 @@ function LoadECDonationData(callback) {
 
 }
 
+function GetPostcodes(data) {
+	var postcodes = [];
+	for (var i = data["Result"].length - 1; i >= 0; i--) {
+		postcodes.push(data["Result"][i]["Postcode"]);
+	}
+	console.log(postcodes);
+	return postcodes.filter(x => x);
+}
