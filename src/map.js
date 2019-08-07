f5f253f3900442e0138e2a6c49594d921f6f0a81
Saul Samuels Moselle <SaulSamuelsMoselle@hotmail.com>
2019-08-07 12:12:47 +0100

    Refactoring, fixed an error in heatmap drawing, tested that querts and queries work

--- src/map.js
+++ src/map.js
@@ -1,22 +1,24 @@
 async function drawHeatmap(map, centresOn = [51.5, 0]) {
-    // Convert postcodes to latlngs, draw heatmap
-    // 1) Get Postcodes
     LoadECDonationData(data => _extractPostcodes(data, async function(d) {
     	const validPostcodeRegex = new RegExp('^(([gG][iI][rR] {0,}0[aA]{2})|((([a-pr-uwyzA-PR-UWYZ][a-hk-yA-HK-Y]?[0-9][0-9]?)|(([a-pr-uwyzA-PR-UWYZ][0-9][a-hjkstuwA-HJKSTUW])|([a-pr-uwyzA-PR-UWYZ][a-hk-yA-HK-Y][0-9][abehmnprv-yABEHMNPRV-Y]))) {0,}[0-9][abd-hjlnp-uw-zABD-HJLNP-UW-Z]{2}))$');
-        var thePostcodes = d.filter(x => x != null).filter(x => validPostcodeRegex.test(x));
+        var thePostcodes = d.filter(x => validPostcodeRegex.test(x));
         var theLatLngs = [];
-        var thePromise;
+        window.errorCounter = 0;
         for (var i = thePostcodes.length - 1; i >= 0; i--) {
             console.log(thePostcodes[i]);
-            let thePromise = fetch("http://api.postcodes.io/postcodes/" + thePostcodes[i])
+            var thePromise = fetch("http://api.postcodes.io/postcodes/" + thePostcodes[i])
                 .then(body => body.json())
-                .then(r => theLatLngs.push([r["result"]["latitude"], r["result"]["longitude"]]));
-            let result = await thePromise;
+                .then(r => {
+                	console.log(r)
+                	theLatLngs.push([r["result"]["latitude"], r["result"]["longitude"]]);
+                })
+                .catch(error => {
+                	console.warn("Found a postcode which no longer resolves: ",error); 
+                	window.errorCounter++;
+                });
+            await thePromise;
         }
-        console.log(theLatLngs);
         var theJSON = GenGeoJSON(theLatLngs);
-        console.log(theJSON);
-
         map.addSource("postcodes", {
             "type": "geojson",
             "data": theJSON
@@ -72,58 +74,6 @@ async function drawHeatmap(map, centresOn = [51.5, 0]) {
             }
         }, 'waterway-label');
     }));
-    map.addLayer({
-        id: 'postcodes-point',
-        type: 'circle',
-        source: 'postcodes',
-        minzoom: 14,
-        paint: {
-            // increase the radius of the circle as the zoom level and dbh value increases
-            'circle-radius': {
-                property: 'dbh',
-                type: 'exponential',
-                stops: [
-                    [{
-                        zoom: 15,
-                        value: 1
-                    }, 5],
-                    [{
-                        zoom: 15,
-                        value: 62
-                    }, 10],
-                    [{
-                        zoom: 22,
-                        value: 1
-                    }, 20],
-                    [{
-                        zoom: 22,
-                        value: 62
-                    }, 50],
-                ]
-            },
-            'circle-color': {
-                property: 'dbh',
-                type: 'exponential',
-                stops: [
-                    [0, 'rgba(236,222,239,0)'],
-                    [10, 'rgb(236,222,239)'],
-                    [20, 'rgb(208,209,230)'],
-                    [30, 'rgb(166,189,219)'],
-                    [40, 'rgb(103,169,207)'],
-                    [50, 'rgb(28,144,153)'],
-                    [60, 'rgb(1,108,89)']
-                ]
-            },
-            'circle-stroke-color': 'white',
-            'circle-stroke-width': 1,
-            'circle-opacity': {
-                stops: [
-                    [14, 0],
-                    [15, 1]
-                ]
-            }
-        }
-    }, 'waterway-label');
 }
 
 mapboxgl.accessToken = 'pk.eyJ1Ijoic2F1bHNtIiwiYSI6ImNqeXpya2s5NTA0ZzEza2xvcTdnbzA5dGEifQ.od-gB5fE_iC0yssElhaCeg';
