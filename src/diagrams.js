ffd603f03db4c922ac21583b63db0e5bfb1f6b6b
Saul Samuels Moselle <SaulSamuelsMoselle@hotmail.com>
2019-08-08 11:13:57 +0100

    Added the beginnings of granular filtering of sankey data

--- src/diagrams.js
+++ src/diagrams.js
@@ -1,36 +1,36 @@
+// TODO: Create customisable granulation
+
 function drawSankeyChart(data) {
-    //creating the data
-    //calling the Sankey function
     var sankey_chart = anychart.sankey(data);//customizing the width of the nodes
     sankey_chart.container("container");//initiating drawing the Sankey diagram
     sankey_chart.draw();
 }
-/*
-anychart.onDocumentReady(function(){
-    var test = [
-        {from: "Google", to: "Facebook", weight: 20000},
-        {from: "Google", to: "Twitter", weight: 17000},
-        {from: "Google", to: "YouTube", weight: 8000},
-        {from: "Google", to: "Wikipedia", weight: 11000},
-        {from: "Bing", to: "Facebook", weight: 7500},
-        {from: "Bing", to: "Twitter", weight: 5000},
-        {from: "Bing", to: "Wikipedia", weight: 4000}
-    ];
-    drawSankeyChart(test);
-});
-*/
+
+
+
 /*Schema: index 1 donee index 5 name index 9 type*/
+function applyGranularFilter(item) {
+    // TODO: Allow this to accept filters - default behaviour is to 
+    // not group political parties but group other things
+    let filteredItem = {
+        from: item[9],
+        weight: currency(item[3])
+    }
+    if (item[2] == "Political Party") {
+        filteredItem["to"] = item[1];
+    } else {
+        filteredItem["to"] = item[2];
+    }
+    return filteredItem;
+}
+
 
 function buildSankeyData(data, fromIndex) {
     // Pass fromIndex = 9 to get results by type ; pass fromIndex = 6 for by individual
     let sankeyItems = [];
     data.forEach(function(item) {
-        console.log(item[fromIndex]);
-        sankeyItems.push({
-            from: item[9],
-            to: item[2],
-            weight: currency(item[3])
-         });   
+        // Granualate here:
+        sankeyItems.push(applyGranularFilter(item));
     });
     return sankeyItems;
 }
